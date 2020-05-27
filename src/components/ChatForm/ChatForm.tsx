f3f79785d023f8a8790bca480d602031d1673c6b
Andrew Hickey <andrewhickey@live.co.uk>
2020-05-27 12:30:26 +0100

    feat: stubs out chatform component

---
+++ src/components/ChatForm/ChatForm.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+type ChatFormProps = {
+  /** smallest possible delay in ms before the form replies */
+  minDelay: number
+  /** longest possible delay in ms before the form replies */
+  maxDelay: number
+}
+
+function ChatForm({ minDelay, maxDelay }: ChatFormProps) {
+  return null
+}
+
+export default ChatForm
